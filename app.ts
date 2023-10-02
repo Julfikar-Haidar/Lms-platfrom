1a3e633bc91ef174ed694d6c2ddc47181a3a7c53
Julfikar-Haidar <julfikar6262@gmail.com>
2023-10-02 09:02:17 +0600

    add model

--- app.ts
+++ app.ts
@@ -1,33 +1,35 @@
-require('dotenv').config()
-import express, {Request, NextFunction, Response } from 'express';
+require("dotenv").config();
+import express, { Request, NextFunction, Response } from "express";
 export const app = express();
-import cors from  'cors'
-import cookieParser from 'cookie-parser';
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
 
 // body parser
-app.use(express.json({limit: '50mb'}))
+app.use(express.json({ limit: "50mb" }));
 
-// cookie parser 
-app.use(cookieParser())
+// cookie parser
+app.use(cookieParser());
 
-app.use(cors({
-    origin: process.env.ORIGIN
-}))
-
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
 
 // tets api
-app.get('/test', (req:Request, res:Response, next:NextFunction) => {
-res.status(200).json({
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
     success: true,
-    message: 'Api working'
-})
-})
+    message: "Api working",
+  });
+});
 
+app.use(ErrorMiddleware);
 
-// unknown route 
-app.all('*', (req:Request, res:Response, next:NextFunction) => {
-    const err = new Error(`Route ${req.originalUrl} not found`) as any
-    err.statusCode = 404
-    next(err)
-})
+// unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
